1a329874a12d0d3f7304e7bac6406618cb132165
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-23 19:31:22 -0500

    clase 4 - fuction scope/complemento clase 3

---
+++ scope/function.js
@@ -0,0 +1,19 @@
+const fruits = () =>{
+
+    var fruit = 'apple';
+    console.log(fruit);
+}
+
+fruits();
+
+const anotherFunction = () =>{
+
+    var x = 1;
+    var x = 2;
+    let y = 1;
+    // let y = 2; Let no permite volver a declarar una variable
+    console.log(x);
+    console.log(y);
+}
+
+anotherFunction();
